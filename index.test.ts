20d4288f6944703745b0446eca894a766a860024
lamothearthur <arthur@shotgun.live>
2024-03-11 12:00:17 +0100

    Created processEvent custom app for US -> EU migration

--- index.test.ts
+++ index.test.ts
@@ -7,30 +7,14 @@ import { processEvent } from "./index";
  * @param $current_url The current url of the page view
  * @returns A new PostHog page view event
  */
-function buildPageViewEvent($current_url: string): PluginEvent {
+function buildEventWithName(eventName: string): PluginEvent {
   const event: PluginEvent = {
-    properties: { $current_url },
     distinct_id: "distinct_id",
     ip: "1.2.3.4",
     site_url: "test.com",
     team_id: 0,
     now: "2022-06-17T20:21:31.778000+00:00",
-    event: "$pageview",
-    uuid: "01817354-06bb-0000-d31c-2c4eed374100",
-  };
-
-  return event;
-}
-
-function buildEventWithoutCurrentUrl(): PluginEvent {
-  const event: PluginEvent = {
-    properties: {},
-    distinct_id: "distinct_id",
-    ip: "1.2.3.4",
-    site_url: "test.com",
-    team_id: 0,
-    now: "2022-06-17T20:21:31.778000+00:00",
-    event: "$identify",
+    event: eventName,
     uuid: "01817354-06bb-0000-d31c-2c4eed374100",
   };
 
@@ -42,102 +26,63 @@ function getMeta(): PluginMeta<PluginInput> {
 }
 
 describe("processEvent", () => {
-  it("shouldn't change a url that's already lowercase", () => {
-    const sourceEvent = buildPageViewEvent("http://www.google.com/test");
+  it("should rename the event if in the list", () => {
+    const sourceEvent = buildEventWithName("Community Management Dashboard Displayed");
 
     const processedEvent = processEvent(sourceEvent, getMeta());
 
-    expect(processedEvent?.properties?.$current_url).toEqual(
-      "http://www.google.com/test"
+    expect(processedEvent?.event).toEqual(
+      "Analytics Community Overview Tab Displayed"
     );
   });
 
-  it("should convert the current_url to lowercase", () => {
-    const sourceEvent = buildPageViewEvent(
-      "http://www.GoOGle.com/WhatAreYouThinking"
-    );
+  it("should rename the event if in the list", () => {
+    const sourceEvent = buildEventWithName("Community Management Insights Overview Tab Displayed");
 
     const processedEvent = processEvent(sourceEvent, getMeta());
 
-    expect(processedEvent?.properties?.$current_url).toEqual(
-      "http://www.google.com/whatareyouthinking"
+    expect(processedEvent?.event).toEqual(
+      "Analytics Community Overview Tab Displayed"
     );
   });
 
-  it("should remove the trailing slash from the current_url", () => {
-    const sourceEvent = buildPageViewEvent(
-      "http://www.google.com/this_is_a_test/"
-    );
+  it("should rename the event if in the list", () => {
+    const sourceEvent = buildEventWithName("Community Management Contacts Imported");
 
     const processedEvent = processEvent(sourceEvent, getMeta());
 
-    expect(processedEvent?.properties?.$current_url).toEqual(
-      "http://www.google.com/this_is_a_test"
+    expect(processedEvent?.event).toEqual(
+      "Marketing Contacts Imported"
     );
   });
 
-  it("should preserve the trailing slash if it's the only character in the path", () => {
-    const sourceEvent = buildPageViewEvent("http://www.google.com/");
+  it("should rename the event if in the list", () => {
+    const sourceEvent = buildEventWithName("Marketing Organizer Page Tab Displayed");
 
     const processedEvent = processEvent(sourceEvent, getMeta());
 
-    expect(processedEvent?.properties?.$current_url).toEqual(
-      "http://www.google.com/"
+    expect(processedEvent?.event).toEqual(
+      "My Page Tab Displayed"
     );
   });
 
-  it("should preserve trailing id anchors", () => {
-    const sourceEvent = buildPageViewEvent(
-      "http://www.google.com/this_is_a_test#id_anchor"
-    );
+  it("should rename the event if in the list", () => {
+    const sourceEvent = buildEventWithName("Settings");
 
     const processedEvent = processEvent(sourceEvent, getMeta());
 
-    expect(processedEvent?.properties?.$current_url).toEqual(
-      "http://www.google.com/this_is_a_test#id_anchor"
+    expect(processedEvent?.event).toEqual(
+      "Profile Screen Settings Tapped"
     );
   });
 
-  it("should preserve trailing anchors but drop trailing slashes", () => {
-    const sourceEvent = buildPageViewEvent(
-      "http://www.google.com/this_is_a_test_with_trailing_slash/#id_anchor"
-    );
+  it("should rename the event if in the list", () => {
+    const sourceEvent = buildEventWithName("Log Out");
 
     const processedEvent = processEvent(sourceEvent, getMeta());
 
-    expect(processedEvent?.properties?.$current_url).toEqual(
-      "http://www.google.com/this_is_a_test_with_trailing_slash#id_anchor"
-    );
-  });
-
-  it("shouldn't modify events that don't have a $current_url set", () => {
-    const sourceEvent = buildEventWithoutCurrentUrl();
-
-    const processedEvent = processEvent(sourceEvent, getMeta());
-
-    expect(processedEvent).toEqual(sourceEvent);
-    expect(processedEvent?.properties).toEqual(sourceEvent.properties);
-    expect(processedEvent?.properties?.$current_url).toBeUndefined();
-  });
-
-  it("should raise an error if the $current_url is an invalid url", () => {
-    const sourceEvent = buildPageViewEvent("invalid url");
-
-    expect(() => processEvent(sourceEvent, getMeta())).toThrowError(
-      `Unable to normalize invalid URL: "invalid url"`
-    );
-  });
-
-  it("should log the normalized_url for debugging", () => {
-    const consoleSpy = jest.spyOn(console, "debug");
-
-    const sourceEvent = buildPageViewEvent(
-      "http://www.GoOGle.com/WhatAreYouThinking"
-    );
-    processEvent(sourceEvent, getMeta());
-
-    expect(consoleSpy).toHaveBeenCalledWith(
-      'event.$current_url: "http://www.GoOGle.com/WhatAreYouThinking" normalized to "http://www.google.com/whatareyouthinking"'
+    expect(processedEvent?.event).toEqual(
+      "Logged Out"
     );
   });
 });
